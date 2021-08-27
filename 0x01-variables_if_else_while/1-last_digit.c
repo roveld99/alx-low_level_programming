01071e38d019d1f15a6ff3d6db48e9d297de8590
roveld99 <roveld99@gmail.com>
2021-08-27 19:23:07 +0300

    variables_if_else_while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,15 +20,15 @@ int n, lastDigit;
 	lastDigit = n % 10;
 if (lastDigit > 5)
 {
-printf("lastdigit of %d is %d and is greater than 5\n", n, lastDigit);
+printf("last digit of %d is %d and is greater than 5\n", n, lastDigit);
 }
 else if (lastDigit < 6 && lastDigit != 0)
 {
-printf("lastdigit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 }
 else
 {
-printf("lastdigit of %d is %d and is 0\n", n, lastDigit);
+printf("last digit of %d is %d and is 0\n", n, lastDigit);
 }
 	return (0);
 }
