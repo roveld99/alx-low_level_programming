e845240f7c48b92d5c919e38df3989f1d40b4881
roveld99 <roveld99@gmail.com>
2021-09-09 16:03:06 +0300

    variables if_else while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,12 +9,8 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		if (letter != 'e' && letter != 'q')
+	for (letter = 'a'; letter <= 'z'; letter++; letter != 'e'; letter != 'q')
 			putchar(letter);
-	}
-
 	putchar('\n');
 
 	return (0);
