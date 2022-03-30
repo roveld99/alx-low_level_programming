43018f30980f27bc38a31ef5b28cfae8fb2c1ebd
roveld99 <roveld99@gmail.com>
2022-03-30 22:09:46 +0300

    search_algorithms

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,76 +1,82 @@
 #include "search_algos.h"
 
 /**
- * print_array - print array with limits in l and r
- * @array: set of numbers
- * @l: left limit
- * @r: right limit
- * Return: nothing
- */
-void print_array(int *array, size_t l, size_t r)
-{
-  printf("Searching in array: ");
-  for (l = l; l < r; l++)
-    printf("%d, ", array[l]);
-  printf("%d\n", array[l]);
-}
-
-/**
- * binary_search_p - search the first ocurrency of a value in the array
- * @array: set of numbers
- * @l: left limit
- * @r: right limit
- * @value: value to search
- * Return: return the first index located otherwise -1
-*/
-int binary_search_p(int *array, size_t l, size_t r, int value)
-{
-size_t mid = 0;
-
-while (l <= r)
-{
-print_array(array, l, r);
-mid = (l + r) / 2;
-if (array[mid] < value)
-l = mid + 1;
-else if (array[mid] > value)
-r = mid - 1;
-else
-return (mid);
-}
-return (-1);
-}
-
-/**
- * min_value - get the min value of two numbers
- * @l: number l
- * @r: number r
- * Return: return the minimun value
- */
-size_t min_value(size_t l, size_t r)
-{
-  return ((l < r) ? l : r);
-}
-
-/**
- * exponential_search - search the first ocurrency of a value in the array
- * @array: set of numbers
+ * exponential_search - searches for a value in an array with
+ * the exponential search algorithm
+ * @array: array to search the value in
  * @size: size of the array
- * @value: value to search
- * Return: return the first index located otherwise -1
-*/
+ * @value: value to look for
+ *
+ * Return: the index of the found value,
+ * or -1 if not found
+ */
 int exponential_search(int *array, size_t size, int value)
 {
-size_t pow = 1;
+	size_t start, end, x;
 
-if (array == NULL)
-return (-1);
-while (pow < size && array[pow] < value)
-{
-printf("Value checked array[%d] = [%d]\n", (int)pow, array[pow]);
-pow *= 2;
+	if (!array || size == 0)
+		return (-1);
+
+	end = 1;
+	start = 1;
+	while (end < size)
+	{
+		if (value < array[end])
+			break;
+		printf("Value checked array[%lu] = [%d]\n", end, array[end]);
+		start = end;
+		end = end * 2;
+	}
+	x = end < size - 1 ? end : size - 1;
+	printf("Value found between indexes [%lu] and [%lu]\n", start, x);
+	return (help_binary(array, value, end / 2, x));
 }
-printf("Value found between indexes [%d] and [%d]\n",
-(int)(pow / 2), (int)min_value(pow, size - 1));
-return (binary_search_p(array, pow / 2, min_value(pow, size - 1), value));
+
+/**
+ * help_binary - searches for a value in an array of
+ * integers using recursion
+ * @array: array to search the value in
+ * @value: value to look for
+ * @lo: index of the low bound
+ * @hi: index of the high bound
+ *
+ * Return: the index of the found value,
+ * or -1 if not found
+ */
+int help_binary(int *array, int value, size_t lo, size_t hi)
+{
+	size_t mid;
+
+	array_print(array, lo, hi);
+	if (hi == lo && array[lo] != value)
+		return (-1);
+
+	mid = ((hi - lo) / 2) + lo;
+	if (array[mid] == value)
+		return (mid);
+	if (array[mid] > value)
+		return (help_binary(array, value, lo, mid - 1));
+	if (array[mid] < value)
+		return (help_binary(array, value, mid + 1, hi));
+	return (-1);
+}
+
+/**
+ * array_print - prints an array
+ * @array: array to print
+ * @lo: index of the low bound
+ * @hi: index of the high bound
+ */
+void array_print(int *array, size_t lo, size_t hi)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = lo; i <= hi; i++)
+	{
+		printf("%d", array[i]);
+		if (i < hi)
+			printf(", ");
+	}
+	printf("\n");
 }
